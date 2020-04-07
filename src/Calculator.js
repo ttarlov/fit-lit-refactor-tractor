f7653818de5a09f6beeba5d1af4a4f348740dd61
Taras Tarlov <ttarlov@gmail.com>
2020-04-07 17:33:46 -0600

    make methods more dynamic

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,14 +5,14 @@ class Calculator {
 
 
   calculateAverageData(fullData, id, property) {
-    let perDayUserData = fullData.filter((data) => id === data.userID);
+    let perDayUserData = this[fullData].filter((data) => id === data.userID);
     return perDayUserData.reduce((sumSoFar, data) => {
       return sumSoFar += data[property];
     }, 0) / perDayUserData.length;
   }
 
   calculateDailyData(fullData, id, date, property) {
-    let findDataByDate = fullData.find((data) => id === data.userID && date === data.date);
+    let findDataByDate = this[fullData].find((data) => id === data.userID && date === data.date);
     return findDataByDate[property];
   }
 
