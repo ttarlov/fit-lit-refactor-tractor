0009e7f0000d6888c1159d3d24c4ae8a2a0d7924
carflor <cfdeclet@gmail.com>
2020-04-12 17:25:30 -0600

    Removed calculateFirstWeekOunces and calculateRandomWeekOunces and inheriting from calc class

--- src/Hydration.js
+++ src/Hydration.js
@@ -15,12 +15,15 @@ class Hydration extends Calculator {
   //   let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
   //   return findOuncesByDate.numOunces;
   // }
-  calculateFirstWeekOunces(userRepo, id) {
-    return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
-  calculateRandomWeekOunces(date, id, userRepo) {
-    return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
+
+  // Translation ONE
+  // calculateFirstWeekOunces(userRepo, id) {
+  //   return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+  // }
+  // Translation TWO
+  // calculateRandomWeekOunces(date, id, userRepo) {
+  //   return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+  // }
 }
 
 
