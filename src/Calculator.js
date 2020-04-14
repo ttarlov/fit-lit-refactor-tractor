a7cf72eacc29abed1a0734609495e9bc7a0cadef
carflor <cfdeclet@gmail.com>
2020-04-13 21:55:24 -0600

    Add randomWeekData methods and calculateWeeklyData

--- src/Calculator.js
+++ src/Calculator.js
@@ -13,7 +13,7 @@ class Calculator {
 
   calculateDailyData(fullData, id, date, property) {
     let findDataByDate = this[fullData].find((data) => id === data.userID && date === data.date);
-    if(findDataByDate === undefined) {
+    if (findDataByDate === undefined) {
       return "0"
     } else {
       return findDataByDate[property];
@@ -26,22 +26,41 @@ class Calculator {
     return calculatedSortedArray
   }
 
-
   // new abstraction
-  calculateWeeklyData(date, id, userStorage, dataSet, property) {
-    return userStorage.getFirstWeek(id, this[dataSet]).map((data) => {
-      `${data[date]}: ${data[property]}`
+  // calculateWeeklyData(date, id, userStorage, dataSet, property) {
+  //   return userStorage.getFirstWeek(id, this[dataSet]).map((data) => {
+  //     `${data[date]}: ${data[property]}`
+  //   });
+  // }
+  // RETRY
+  calculateWeeklyData(userRepo, id, dataSet, property) {
+    return userRepo.getFirstWeek(id, this[dataSet]).map((data) => {
+      `${data.date}: ${data[property]}`
     });
   }
 
-  calculateRandomWeekData(date, id, userStorage, dataSet, property) {
-    console.log('date', date)
-    console.log('id', id)
-    console.log('userRepo', userStorage)
-    console.log('data', dataSet)
-    console.log('ounces', property)
-    return userStorage.getWeekFromDate(date, id, this[dataSet]).map((data) => `${data[date]}: ${data[property]}`);
+
+   // calculateFirstWeekOunces(userRepo, id) {
+  //   return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+  // }
+
+  calculateRandomWeekData(date, id, userRepo, dataSet, property) {
+    if (dataSet === undefined) {
+      return "0"
+    } else {
+      return userRepo.getWeekFromDate(date, id, this[dataSet]).map((data) => `${data.date}: ${data[property]}`);
+    }
   }
+
+
+  // if (dataSet === undefined) {
+  //   return "0"
+  // } else {
+  //   return userRepo.getWeekFromDate();
+  // }
+
+
+
 }
 
 export default Calculator
